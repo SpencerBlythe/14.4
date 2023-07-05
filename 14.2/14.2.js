5ba6f9c5e26fe3526f2e911c7aabacf96af38729
SpencerBlythe <121988330+SpencerBlythe@users.noreply.github.com>
2023-07-05 12:06:16 -0400

    Delete 14.2 directory

--- 14.2/14.2.js
+++
@@ -1,6 +0,0 @@
-const PI = 3.14;
-PI = 42;
-
-//you can reassign and redeclare with var, you can reassign with let, and you can do either with const. you can access a hoisted variable with var. let and const create block scope.
-
-//hoisting is when variables are lifted to the top of the scope they are declared in.
